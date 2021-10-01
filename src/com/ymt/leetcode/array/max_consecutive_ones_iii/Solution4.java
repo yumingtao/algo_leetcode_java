dd7eb9597e20c77b2c4ade5794a1f62dc090308a
yumingtao <yumingtao2008@gmail.com>
2021-10-01 21:39:18 +0800

    Add solutions for max consecutive ones iii.

---
+++ src/com/ymt/leetcode/array/max_consecutive_ones_iii/Solution4.java
@@ -0,0 +1,32 @@
+package com.ymt.leetcode.array.max_consecutive_ones_iii;
+
+/**
+ * 前缀和+双指针优化
+ *
+ * @author yumingtao
+ * @date 2021/10/1 21:38
+ */
+public class Solution4 {
+    public int longestOnes(int[] nums, int k) {
+        int ans = Integer.MIN_VALUE;
+        int lSum = 0;
+        int rSum = 0;
+        int l = 0;
+
+        //preSum[l]>=preSum[r+1]-k
+        for (int r = 0; r < nums.length; r++) {
+            //注意此处1-nums[i-1]是把1变成0，0变成1
+            rSum += 1 - nums[r];
+
+            //说明l小了，需要++
+            while (lSum < rSum - k) {
+                lSum += 1 - nums[l];
+                l++;
+            }
+
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+}
