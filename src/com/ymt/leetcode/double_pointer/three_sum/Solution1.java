b9980eeba11aa61a86ff3c07e9bc6bd2d41b8b40
yumingtao <yumingtao2008@gmail.com>
2021-11-11 10:30:03 +0800

    Add solutions for 3sum.

---
+++ src/com/ymt/leetcode/double_pointer/three_sum/Solution1.java
@@ -0,0 +1,57 @@
+package com.ymt.leetcode.double_pointer.three_sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 思路: 利用两数之和
+ * 1. i<j<k, nums[i]+nums[j]+nums[k]=0
+ * 2. 固定一端i，则有nums[j]+nums[k]=-nums[i]，对数组在[j,k]，求满足两数之和等于-nums[i]的所有组合
+ * 3. 注意排重
+ *
+ * @author yumingtao
+ * @date 2021/11/11 09:17
+ */
+public class Solution1 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        //首先对数组排序
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            //防止元素重复
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            List<List<Integer>> results = twoSum(nums, i + 1, 0 - nums[i]);
+            for (List<Integer> res : results) {
+                ans.add(res);
+            }
+        }
+        return ans;
+    }
+
+    private List<List<Integer>> twoSum(int[] nums, int start, int target) {
+        int j = nums.length - 1;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = start; i < j; i++) {
+            //防止元素重复
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            while (i < j && nums[i] + nums[j] > target) {
+                j--;
+            }
+
+            if (i < j && nums[i] + nums[j] == target) {
+                List<Integer> list = new ArrayList<>();
+                list.add(0 - target);
+                list.add(nums[i]);
+                list.add(nums[j]);
+                ans.add(list);
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ src/com/ymt/leetcode/double_pointer/three_sum/Solution2.java
@@ -0,0 +1,36 @@
+package com.ymt.leetcode.double_pointer.three_sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 思路:暴力解法（超时）
+ *
+ * @author yumingtao
+ * @date 2021/11/11 10:25
+ */
+public class Solution2 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        //首先对数组排序
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (k > j + 1 && nums[k] == nums[k - 1]) continue;
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        ans.add(temp);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
