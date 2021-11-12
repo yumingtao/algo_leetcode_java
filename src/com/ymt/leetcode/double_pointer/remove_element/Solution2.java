402e1133b856fae497a2fae2c59602e575bfa4ed
yumingtao <yumingtao2008@gmail.com>
2021-11-12 16:54:35 +0800

    Add solutions for remove element.

---
+++ src/com/ymt/leetcode/double_pointer/remove_element/Solution2.java
@@ -0,0 +1,24 @@
+package com.ymt.leetcode.double_pointer.remove_element;
+
+/**
+ * 思路：对撞指针
+ *
+ * @author yumingtao
+ * @date 2021/11/12 16:43
+ */
+public class Solution2 {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right];
+                right--;
+            }else {
+                left++;
+            }
+        }
+
+        return left;
+    }
+}
---
+++ src/com/ymt/leetcode/double_pointer/remove_element/Solution3.java
@@ -0,0 +1,19 @@
+package com.ymt.leetcode.double_pointer.remove_element;
+
+/**
+ * 思路：朴素解法
+ *
+ * @author yumingtao
+ * @date 2021/11/12 16:50
+ */
+public class Solution3 {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for(int num : nums){
+            if(num != val){
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
+}
