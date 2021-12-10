0f2eb16735e0321f5596a48ac1f777c26f325c18
yumingtao <yumingtao2008@gmail.com>
2021-12-10 18:07:17 +0800

    Add solution1 for max consecutive ones iii.

---
+++ src/com/ymt/leetcode/two_pointers/max_consecutive_ones_iii/Solution1.java
@@ -0,0 +1,42 @@
+package com.ymt.leetcode.two_pointers.max_consecutive_ones_iii;
+
+/**
+ * 思路:
+ *
+ * @author yumingtao
+ * @date 2021/12/10 16:13
+ */
+public class Solution1 {
+    public int longestOnes(int[] nums, int k) {
+        int len = nums.length;
+        int zeroCount = 0;
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        while (right < len) {
+            //right向右一直到0的个数大于k
+            while (right < len && zeroCount <= k) {
+                if (nums[right] == 0) {
+                    ++zeroCount;
+                }
+                if (zeroCount <= k) {
+                    //更新maxLen
+                    maxLen = Math.max(maxLen, right - left + 1);
+                }
+                ++right;
+            }
+            //如果right先走到末尾，直接返回maxLen
+            if (right == maxLen) {
+                return maxLen;
+            }
+            //right没有走到末尾，zeroCont>k,收缩left，直到zeroCount<=k,收缩过程中不需要更新maxLen
+            while (left <= right && zeroCount > k) {
+                if (nums[left] == 0) {
+                    --zeroCount;
+                }
+                ++left;
+            }
+        }
+        return maxLen;
+    }
+}
