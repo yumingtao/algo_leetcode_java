39f8b3c02479300aa1614e811cbfd353b3b6a478
yumingtao <yumingtao2008@gmail.com>
2022-04-20 17:39:15 +0800

    Add solution4 for remove linked list elements.

--- src/com/ymt/leetcode/remove_linked_list_elements/Solution4.java
+++ src/com/ymt/leetcode/remove_linked_list_elements/Solution4.java
@@ -6,4 +6,20 @@ package com.ymt.leetcode.remove_linked_list_elements;
  * @date 2022/4/19 13:40
  */
 public class Solution4 {
+    public ListNode removeElements(ListNode head, int val) {
+        //排除头节点val等于val的情况
+        while(head != null && head.val == val){
+            head = head.next;
+        }
+        //此时head如果不是null，那么head.val一定不是val
+        ListNode node = head;
+        while(head != null && head.next != null){
+            if(head.next.val == val){
+                head.next = head.next.next;
+            }else {
+                head = head.next;
+            }
+        }
+        return node;
+    }
 }
