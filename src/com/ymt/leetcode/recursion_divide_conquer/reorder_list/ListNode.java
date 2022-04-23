d44f51b169c4f28b6925ec20ff9b9219514fa39d
yumingtao <yumingtao2008@gmail.com>
2022-04-23 21:19:16 +0800

    Add solutions for reorder list.

---
+++ src/com/ymt/leetcode/recursion_divide_conquer/reorder_list/ListNode.java
@@ -0,0 +1,23 @@
+package com.ymt.leetcode.recursion_divide_conquer.reorder_list;
+
+/**
+ * @Description
+ * @author yumingtao
+ * @date 2022-04-23 15:46
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
