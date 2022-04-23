012933b5022cab37f0947c40b9150e1bb2e98ac5
yumingtao <yumingtao2008@gmail.com>
2022-04-23 15:14:39 +0800

    Add solutions for remove linked list elements.

---
+++ src/com/ymt/leetcode/remove_linked_list_elements/Solution5.java
@@ -0,0 +1,24 @@
+package com.ymt.leetcode.remove_linked_list_elements;
+
+/**
+ * @Description
+ * @author yumingtao
+ * @date 2022-04-23 15:07
+ */
+public class Solution5 {
+    public ListNode removeElements(ListNode head, int val) {
+        //新建一个虚拟节点指向head，来处理head的第一个节点的val就等于参数中val的情况
+        ListNode prorect = new ListNode(-1);
+        prorect.next = head;
+        //新建一个节点指向protect，做为迭代指针遍历protect
+        ListNode node = prorect;
+        while (node.next != null) {
+            if (node.next.val == val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return prorect.next;
+    }
+}
