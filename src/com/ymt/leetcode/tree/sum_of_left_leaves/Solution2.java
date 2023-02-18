6fa29ca67ccebc0445fb73b633fcac4cda378ec1
yumingtao <yumingtao2008@gmail.com>
2023-02-18 17:07:19 +0800

    Add solution2 for sum of left leaves.

---
+++ src/com/ymt/leetcode/tree/sum_of_left_leaves/Solution2.java
@@ -0,0 +1,28 @@
+public class Solution2 {
+    private int sum = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        return root == null ? 0 : travel(root);
+    }
+
+    private int travel(TreeNode root){
+       int ans = 0;
+       if(root.left != null){
+           if(isLeaf(root.left)){
+                ans += root.left.val;
+           }else{
+                ans += travel(root.left);
+           }
+       }
+
+       if(root.right != null){
+           if(!isLeaf(root.right)){
+               ans += travel(root.right);
+           }
+       }
+       return ans;
+    }
+
+    private boolean isLeaf(TreeNode node){
+        return node.left == null && node.right == null;
+    }
+}
